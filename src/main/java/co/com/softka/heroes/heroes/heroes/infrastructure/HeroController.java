76868b8857bb6eea74f5822acc5469071e53b54c
juan carlos salcedo <juank9225@gmail.com>
2021-07-16 10:27:09 -0500

    Se agrega nuevamente el empoint para trabajar local el front y hacer modificaciones

--- src/main/java/co/com/softka/heroes/heroes/heroes/infrastructure/HeroController.java
+++ src/main/java/co/com/softka/heroes/heroes/heroes/infrastructure/HeroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-heros.web.app")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/heroes")
 class HeroController {
 
